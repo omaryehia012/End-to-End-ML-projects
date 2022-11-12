0463274f4ffe541ac0b6ee1961d2aaf8cfa0f135
Omar Mohamed Yehia <93586279+omaryehia012@users.noreply.github.com>
2022-11-12 20:15:44 +0200

    Update app.py

--- Zomato_Resturants_Classfiction/app.py
+++ Zomato_Resturants_Classfiction/app.py
@@ -1,11 +1,13 @@
 import joblib
 import streamlit as st
+import pickle
 from variables import *
  
 # loading the trained model
-scaler = joblib.load(open('scaler.h5','rb'))
-model = joblib.load(open('Zomato Restaurants.h5','rb'))
-
+#scaler = joblib.load(open('scaler.h5','rb'))
+#model = joblib.load(open('Zomato Restaurants.h5','rb'))
+scaler = pickle.load(open('scaler.h5','rb'))
+model = pickle.load(open('model.h5','rb'))
  
 @st.cache()
   
